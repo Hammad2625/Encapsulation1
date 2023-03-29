f46e513d81aafe6491a78ab44154b8b103835b22
Hammad2625 <68948000+Hammad2625@users.noreply.github.com>
2023-03-29 13:20:00 +0500

    Add project files.

---
+++ Name.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeterSeter
+{
+    internal class Name
+    {
+        private string _firstname;
+        private string _lastname;
+        private int _age;
+
+        public string Firstname
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        public string Lastname
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+        public int Age
+        { get { return _age; }
+          set {_age = value; } 
+        }
+
+        public string Naam()
+        {
+            return _firstname+" "+ _lastname+" "+ _age;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeterSeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Name name = new Name();
+            name.Firstname = "Hammad";
+            name.Lastname = "Khalid";
+            name.Age = 23;
+            Console.WriteLine(name.Naam());
+        }
+    }
+}
